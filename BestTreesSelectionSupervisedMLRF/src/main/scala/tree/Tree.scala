fc457dbfcbf1cdd3eeb968906c7b5e2c82669c9d
ricardmc99 <ricardmc99@gmail.com>
2020-10-25 16:43:44 -0400

    Clean up

--- BestTreesSelectionSupervisedMLRF/src/main/scala/tree/Tree.scala
+++
@@ -1,8 +0,0 @@
-package tree
-
-import org.apache.spark.mllib.tree.model.DecisionTreeModel
-
-class Tree(acc:Double, dtm: DecisionTreeModel) extends Serializable {
-  var accuracy = acc
-  var treeModel = dtm
-}
